ba256dfc12cc269c97b6a7e202d413f614420f10
thomas.diesler@jboss.com <thomas.diesler@jboss.com>
2007-05-15 05:13:16 +0000

    restructure

---
+++ src/main/java/org/jboss/ws/integration/deployment/SecurityRolesHandler.java
@@ -0,0 +1,10 @@
+package org.jboss.ws.integration.deployment;
+
+import org.w3c.dom.Element;
+
+public interface SecurityRolesHandler
+{
+   /** Add the roles from ejb-jar.xml to the security roles
+    */
+   void addSecurityRoles(Element webApp, UnifiedDeploymentInfo udi);
+}
