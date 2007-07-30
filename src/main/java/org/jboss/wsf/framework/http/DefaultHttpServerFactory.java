769abd39cf575267513a92e7cc7571fe47845994
thomas.diesler@jboss.com <thomas.diesler@jboss.com>
2007-07-30 12:53:36 +0000

    Add missing HTTPServerFactory

---
+++ src/main/java/org/jboss/wsf/framework/http/DefaultHttpServerFactory.java
@@ -0,0 +1,43 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.wsf.framework.http;
+
+//$Id: HttpContext.java 1757 2006-12-22 15:40:24Z thomas.diesler@jboss.com $
+
+import org.jboss.wsf.common.KernelAwareSPIFactory;
+import org.jboss.wsf.spi.http.HttpServer;
+import org.jboss.wsf.spi.http.HttpServerFactory;
+
+/**
+ * A factory for an abstract HTTP Context
+ *
+ * @author Thomas.Diesler@jboss.org
+ * @since 07-Jul-2006
+ */
+public class DefaultHttpServerFactory extends HttpServerFactory
+{
+   @Override
+   public HttpServer getHttpServer()
+   {
+      return new KernelAwareSPIFactory().getKernelProvidedSPI(HttpServer.BEAN_NAME, HttpServer.class);
+   }
+}
