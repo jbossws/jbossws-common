c6e4b58aa1e89be68245ccf2401c33bb676164c9
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2009-12-10 09:50:46 +0000

    [JBPAPP-3241] new Normalizer

--- src/main/java/org/jboss/wsf/common/DOMWriter.java
+++ src/main/java/org/jboss/wsf/common/DOMWriter.java
@@ -573,61 +573,10 @@ public class DOMWriter
       }
       return (array);
    }
-
+   
    /** Normalizes the given string. */
    public static String normalize(String s, boolean canonical)
    {
-      StringBuffer str = new StringBuffer();
-
-      int len = (s != null) ? s.length() : 0;
-      for (int i = 0; i < len; i++)
-      {
-         char ch = s.charAt(i);
-         switch (ch)
-         {
-            case '<':
-            {
-               str.append("&lt;");
-               break;
-            }
-            case '>':
-            {
-               str.append("&gt;");
-               break;
-            }
-            case '&':
-            {
-               str.append("&amp;");
-               break;
-            }
-            case '"':
-            {
-               str.append("&quot;");
-               break;
-            }
-            case '\'':
-            {
-               str.append("&apos;");
-               break;
-            }
-            case '\r':
-            case '\n':
-            {
-               if (canonical)
-               {
-                  str.append("&#");
-                  str.append(Integer.toString(ch));
-                  str.append(';');
-                  break;
-               }
-               // else, default append char
-            }
-            default:
-            {
-               str.append(ch);
-            }
-         }
-      }
-      return (str.toString());
+      return Normalizer.normalize(s, canonical);
    }
 }
