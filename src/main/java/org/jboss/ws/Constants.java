03bf62ec96a56e3ac6c39d95b13bd0d4f76e9bf0
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2010-04-07 13:59:41 +0000

    [JBWS-2974] Removing constant for lazy load

--- src/main/java/org/jboss/ws/Constants.java
+++ src/main/java/org/jboss/ws/Constants.java
@@ -328,6 +328,4 @@ public interface Constants
    static final String NETTY_MESSAGE = "org.jboss.ws.http.netty.Message";
 
    static final boolean BC_CONTEXT_MODE = Boolean.parseBoolean(System.getProperty("org.jboss.ws.backward_compatible_context_creator", "false"));
-   
-   static final boolean LAZY_LOAD_CXF_BUS = Boolean.parseBoolean(System.getProperty("org.jboss.ws.lazyLoadCxfBus", "false"));
 }
