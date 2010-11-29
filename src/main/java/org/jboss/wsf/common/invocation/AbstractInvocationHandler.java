5bfe57ab29a8ea8cdbe997e73d2644747887b6da
richard.opalka@jboss.com <richard.opalka@jboss.com>
2010-11-29 10:44:55 +0000

    [JBWS-3171] refactoring lifecycle methods one level down

--- src/main/java/org/jboss/wsf/common/invocation/AbstractInvocationHandler.java
+++ src/main/java/org/jboss/wsf/common/invocation/AbstractInvocationHandler.java
@@ -92,4 +92,38 @@ public abstract class AbstractInvocationHandler extends InvocationHandler
       return implClass.getMethod(methodName, paramTypes);
    }
 
+   /**
+    * Template method for notifying subclasses that endpoint instance have been instantiated.
+    *
+    * @param endpoint instantiated endpoint
+    * @param invocation current invocation
+    * @throws Exception subclasses have to throw exception on any failure
+    */
+   protected void onEndpointInstantiated(final Endpoint endpoint, final Invocation invocation) throws Exception
+   {
+      // does nothing
+   }
+
+   /**
+    * Template method for notifying subclasses that endpoint method is going to be invoked.
+    *
+    * @param invocation current invocation
+    * @throws Exception subclasses have to throw exception on any failure
+    */
+   protected void onBeforeInvocation(final Invocation invocation) throws Exception
+   {
+      // does nothing
+   }
+
+   /**
+    * Template method for notifying subclasses that endpoint method invocation was completed.
+    *
+    * @param invocation current invocation
+    * @throws Exception subclasses have to throw exception on any failure
+    */
+   protected void onAfterInvocation(final Invocation invocation) throws Exception
+   {
+      // does nothing
+   }
+
 }
