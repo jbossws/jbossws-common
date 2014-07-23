9f9928410d92e5429e413a0fab7d25fbdeab3538
asoldano <asoldano>
2014-07-23 08:06:33 +0000

    Adding missing header

--- src/main/java/org/jboss/ws/common/utils/UrlPatternUtils.java
+++ src/main/java/org/jboss/ws/common/utils/UrlPatternUtils.java
@@ -1,3 +1,24 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.ws.common.utils;
 
 import org.jboss.ws.api.annotation.WebContext;
@@ -51,7 +72,7 @@ public class UrlPatternUtils {
         return null;
     }
 
-    public static String getUrlPatternByPortComponentURI (String urlPattern, String contextRoot) {
+    public static String getUrlPatternByPortComponentURI(String urlPattern, String contextRoot) {
 
         if (urlPattern != null)
         {
@@ -68,7 +89,7 @@ public class UrlPatternUtils {
     }
 
 
-    public static String getUrlPatternByPortComponentURI (JBossPortComponentMetaData portComponent) {
+    public static String getUrlPatternByPortComponentURI(JBossPortComponentMetaData portComponent) {
         String urlPattern = null;
         if (portComponent != null) {
             String portComponentURI = portComponent.getPortComponentURI();
